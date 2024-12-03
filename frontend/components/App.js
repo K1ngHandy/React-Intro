dec2d318424c8fd84978bcb179cc31b1e96afd40
K1ngHandy <bolds-slicks0g@icloud.com>
2024-12-02 23:42:44 -0500

    add emoji

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>
-      <h2>Hello, World!</h2>
+    <div className='App'>
+      <p>
+        😄
+      </p>
     </div>
   )
 }
