a8a97d1875553d1a9d62962190a601512f76c12e
K1ngHandy <bolds-slicks0g@icloud.com>
2024-12-05 09:49:45 -0500

    add state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 
-const emoji = "😄"
+const happyEmoji = "😄";
+const sadEmoji = "😞";
 const titleObj = {
-  title: "Mood:"
+  title: "Current Mood:"
 }
 const greeting = "Hello learners!"
 
 export default function App() {
+  const [ happy, setHappy ] = useState(true);
+
   return (
-    <div className='App'>
+    <div className="App">
       <h1>{greeting}</h1>
-      <h2>{titleObj.title} {emoji}</h2>
+      {happy === false ? <h2>{titleObj.title} {sadEmoji}</h2> : <h2>{titleObj.title} {happyEmoji}</h2>}
+
       {/* <p>
         {emoji}
       </p> */}
