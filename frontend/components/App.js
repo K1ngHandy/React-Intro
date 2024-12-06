fdce65134fef30e38b46722bd1a7f26b55dfeb87
K1ngHandy <bolds-slicks0g@icloud.com>
2024-12-05 22:23:56 -0500

    change welcome phrase, add click listener

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,22 +3,13 @@ import { useState } from 'react';
 
 const happyEmoji = "😄";
 const sadEmoji = "😞";
-const titleObj = {
-  title: "Current Mood:"
-}
-const greeting = "Hello learners!"
 
 export default function App() {
   const [ happy, setHappy ] = useState(true);
 
   return (
-    <div className="App">
-      <h1>{greeting}</h1>
-      {happy === false ? <h2>{titleObj.title} {sadEmoji}</h2> : <h2>{titleObj.title} {happyEmoji}</h2>}
-
-      {/* <p>
-        {emoji}
-      </p> */}
+    <div className="App" onClick={ () => setHappy(!happy) }>
+      {happy === false ? <h1>Hello {sadEmoji} learners...</h1> : <h1>Hello {happyEmoji} learners!</h1>}
     </div>
   )
 }
